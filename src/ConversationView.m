34b07b6aaa142e4c5a64efeb9011c47f20611c08
Qusic <QusicS@Me.com>
2015-03-30 21:27:06 +0800

    simplify conversation view controller

--- src/ConversationView.m
+++ src/ConversationView.m
@@ -8,41 +8,34 @@
     self.view.backgroundColor = [UIColor clearColor];
 }
 
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
-{
-    if (self.conversation) {
-        return [super collectionView:collectionView numberOfItemsInSection:section];
-    } else {
-        //TODO: third party apps
-        return 0;
-    }
-}
-
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    CKTranscriptCell *cell;
-    if (self.conversation) {
-        cell = (CKTranscriptCell *)[super collectionView:collectionView cellForItemAtIndexPath:indexPath];
-    } else {
-        //TODO: third party apps
-    }
-    if ([cell isKindOfClass:CKTranscriptBalloonCell.class]) {
-        CKBalloonView *balloonView = ((CKTranscriptBalloonCell *)cell).balloonView;
-        balloonView.canUseOpaqueMask = NO;
-        balloonView.filled = NO;
-        if ([balloonView isKindOfClass:CKColoredBalloonView.class]) {
-            CKColoredBalloonView *coloredBalloonView = (CKColoredBalloonView *)balloonView;
-            coloredBalloonView.color = CKBalloonColorWhite;
-            if ([balloonView isKindOfClass:CKTextBalloonView.class]) {
-                CKTextBalloonView *textBalloonView = (CKTextBalloonView *)coloredBalloonView;
-                NSMutableAttributedString *text = textBalloonView.attributedText.mutableCopy;
-                [text addAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]} range:NSMakeRange(0, text.length)];
-                [text removeAttribute:NSLinkAttributeName range:NSMakeRange(0, text.length)];
-                textBalloonView.attributedText = text;
-            }
+    CKTranscriptCell *cell = (CKTranscriptCell *)[super collectionView:collectionView cellForItemAtIndexPath:indexPath];
+    if ([cell isKindOfClass:CKTranscriptMessageCell.class]) {
+        CKTranscriptMessageCell *messageCell = (CKTranscriptMessageCell *)cell;
+        if (self.conversation == nil) {
+            messageCell.wantsContactImageLayout = NO;
+            messageCell.contactImage = nil;
         }
-        [balloonView setNeedsPrepareForDisplay];
-        [balloonView prepareForDisplayIfNeeded];
+        if ([messageCell isKindOfClass:CKTranscriptBalloonCell.class]) {
+            CKTranscriptBalloonCell *balloonCell = (CKTranscriptBalloonCell *)cell;
+            CKBalloonView *balloonView = balloonCell.balloonView;
+            balloonView.canUseOpaqueMask = NO;
+            balloonView.filled = NO;
+            if ([balloonView isKindOfClass:CKColoredBalloonView.class]) {
+                CKColoredBalloonView *coloredBalloonView = (CKColoredBalloonView *)balloonView;
+                coloredBalloonView.color = CKBalloonColorWhite;
+                if ([balloonView isKindOfClass:CKTextBalloonView.class]) {
+                    CKTextBalloonView *textBalloonView = (CKTextBalloonView *)coloredBalloonView;
+                    NSMutableAttributedString *text = textBalloonView.attributedText.mutableCopy;
+                    [text addAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]} range:NSMakeRange(0, text.length)];
+                    [text removeAttribute:NSLinkAttributeName range:NSMakeRange(0, text.length)];
+                    textBalloonView.attributedText = text;
+                }
+            }
+            [balloonView setNeedsPrepareForDisplay];
+            [balloonView prepareForDisplayIfNeeded];
+        }
     }
     return cell;
 }
